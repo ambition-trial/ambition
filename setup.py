bf24a33f6815b2871a9ea72371ea72c63a4726e3
erikvw <ew2789@gmail.com>
2018-07-05 13:24:03 +0200

    correct org name

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     author_email='ew2789@gmail.com',
     packages=find_packages(),
     include_package_data=True,
-    url='https://github.com/ambition-trail/ambition',
+    url='https://github.com/ambition-trial/ambition',
     license='GPL license, see LICENSE',
     description='ambition',
     long_description=README,
