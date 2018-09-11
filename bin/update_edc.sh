4a961010437837fe840e4edba1a6cc74e79f8f0f
erikvw <ew2789@gmail.com>
2018-09-11 12:47:12 -0500

    update edc script

--- bin/update_edc.sh
+++ bin/update_edc.sh
@@ -13,17 +13,17 @@ while true; do
     esac
 done
 while true; do
-    read -p "Update edc permissions? [y/n]" yn
+    read -p "Run migrations? [y/n]" yn
     case $yn in
-        [Yy]* ) update_permissions="y"; break;;
+        [Yy]* ) migrate="y"; break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
 while true; do
-    read -p "Run migrations? [y/n]" yn
+    read -p "Update edc permissions? [y/n]" yn
     case $yn in
-        [Yy]* ) migrate="y"; break;;
+        [Yy]* ) update_permissions="y"; break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
@@ -36,7 +36,7 @@ while true; do
         * ) echo "Please answer yes or no.";;
     esac
 done
-echo "${green}Updating ENV ... ${reset}"
+echo "${green}Start ... ${reset}"
 
 if [ "${update_ubuntu}" = "y" ]; then
   echo "${green}Updating permissions ... ${reset}"
@@ -45,20 +45,22 @@ if [ "${update_ubuntu}" = "y" ]; then
 fi
 
 cd ~/app \
+  && git checkout master \
   && git pull \
   && . ~/.venvs/ambition/bin/activate \
   && pip install --no-cache-dir -U -r requirements/stable.txt \
   && pip install -e .
 
- if [ "${update_permissions}" = "y" ]; then
-  echo "${green}Updating permissions ... ${reset}"
-  cd ~/app \
-  && python manage.py update_edc_permissions
-fi
 if [ "${migrate}" = "y" ]; then
   echo "${green}Running migrations ... ${reset}"
   cd ~/app \
   && python manage.py migrate
+fi
+
+ if [ "${update_permissions}" = "y" ]; then
+  echo "${green}Updating permissions ... ${reset}"
+  cd ~/app \
+  && python manage.py update_edc_permissions
 fi
 
 echo "${green}Restarting gunicorn / gunicorn-uat ... ${reset}"
